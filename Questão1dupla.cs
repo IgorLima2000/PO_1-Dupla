8b993c2b27d9c4b770ab43e1bbf7b31c60389c20
HenriqueRamosdaSilva <146762258+HenriqueRamosdaSilva@users.noreply.github.com>
2025-02-17 22:17:49 -0300

    Add files via upload

---
+++ Questão1dupla.cs
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+                            Online C# Compiler.
+                Code, Compile, Run and Debug C# program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+using System;
+class tipoprimitivo {
+  static void Main() {
+    int a = 5;
+    int b = a;
+    b = 21;
+    Console.WriteLine(a);
+    Console.WriteLine(b);
+    
+  }
+}
